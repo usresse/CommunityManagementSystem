35d309e8da13b85d387504ba10a62c143e39aeab
hyc <hyc@qq.com>
2022-09-29 22:06:45 +0800

    将菜单的iframe去除合并到page页面

--- src/main/resources/static/js/page.js
+++ src/main/resources/static/js/page.js
@@ -53,3 +53,67 @@ $(function () {
     }
 )
 
+/******************菜单**************************/
+/**更改函数数据*/
+$(function () {
+    /**对成员隐藏审核选项*/
+    let data = personalBean["major"];
+
+    /**成员菜单*/
+    if (data == "3") {
+        $(".ti3").hide();
+        $(".tem2 > div > div").eq(0).text("其他成员");
+    }
+
+    /**社长菜单*/
+    if (data == "1") {
+        $(".tem3 > div").prepend($(".tem2 > div > div").eq(0).text("社团成员管理"));
+        /**添加社团信息维护*/
+        $(".tem3").append($("<div><div onclick=JumpHtml('CommunityInformationMaintenance')>社团信息维护</div></div>"))
+    }
+
+    if (data == "" || data == null || data.length == 0) {
+        $(".ti3").hide();
+        /**隐藏参加的社团活动申请和历史*/
+        $(".tem2").find("div").eq(2).hide()
+        $(".tem2").find("div").eq(3).hide();
+    } else {
+        $(".tem3").append($("<div><div onclick=JumpHtml('notice')>社团通知</div></div>"))
+    }
+
+    /**对社团申请的菜单隐藏*/
+    if (data != null && data != "") {
+        $(".nul").hide();
+    }
+});
+
+
+/**菜单框效果实现*/
+function toggleshow(cla) {
+    $("."+cla).toggle(1);
+};
+
+/**设置点击事件页面跳转*/
+function JumpHtml(url){
+    $("#nei").attr("src", url);
+}
+
+function JumpHtmlAndNumber(url) {
+    $("#nei").attr("src", url + "/" + personalBean["number"]);
+}
+
+/************************菜单样式*****************************************************/
+$(function (){
+    /* 一级标题样式 */
+    $(".stair").on("click",function(){
+        $(".stair").css({"background-color": "aqua","color":"#000"});
+        $(this).css({"background-color": "#000","color":"#fff"});
+    });
+
+    /* 二级标题样式 */
+    $(".stair2 > div > div").on("click",function(){
+        $(".stair2   > div > div").css({"background-color": "#27eaff","color":"#000"});
+        $(this).css({"background-color": "#000","color":"#fff"});
+    });
+});
+
