55eaf0b2b1eb651dcd832f2b43b979d2e806e327
hyc <hyc@qq.com>
2022-09-29 20:54:41 +0800

    主页面样式优化

--- src/main/resources/static/js/caidan.js
+++ src/main/resources/static/js/caidan.js
@@ -7,8 +7,8 @@ var number = personalBean["number"];
 var ifram = $("#nei",parent.document);
 
 
+/**更改函数数据*/
 $(function () {
-
 
     /**对成员隐藏审核选项*/
     let data = personalBean["major"];
@@ -16,14 +16,14 @@ $(function () {
     /**成员菜单*/
     if (data == "3") {
         $(".ti3").hide();
-        $(".tem2>div").eq(0).text("其他成员");
+        $(".tem2 > div > div").eq(0).text("其他成员");
     }
 
     /**社长菜单*/
     if (data == "1") {
-        $(".tem3").prepend($(".tem2 > div").eq(0).text("社团成员管理"))
+        $(".tem3 > div").prepend($(".tem2 > div > div").eq(0).text("社团成员管理"));
         /**添加社团信息维护*/
-        $(".tem3").append($("<div onclick=CommunityInformationMaintenance('CommunityInformationMaintenance')></div>").text("社团信息维护"))
+        $(".tem3").append($("<div><div onclick=CommunityInformationMaintenance('CommunityInformationMaintenance')>社团信息维护</div></div>"))
     }
 
     if (data == "" || data == null || data.length == 0) {
@@ -32,7 +32,7 @@ $(function () {
         $(".tem2").find("div").eq(2).hide()
         $(".tem2").find("div").eq(3).hide();
     } else {
-        $(".tem3").append($("<div onclick=notice('notice')>社团通知</div>"))
+        $(".tem3").append($("<div><div onclick=notice('notice')>社团通知</div></div>"))
     }
 
     /**对社团申请的菜单隐藏*/
@@ -44,7 +44,7 @@ $(function () {
 
 /**菜单框效果实现*/
 function toggleshow(cla) {
-    $(document.getElementsByClassName(cla)).toggle(1);
+    $("."+cla).toggle(1);
 };
 
 /**设置点击事件页面跳转*/
@@ -96,3 +96,18 @@ function announcement(url) {
 function notice(url) {
     ifram.attr("src", url);
 }
+
+/*****************************************************************************/
+$(function (){
+    /* 一级标题样式 */
+    $(".stair").on("click",function(){
+        $(".stair").css({"background-color": "aqua","color":"#000"});
+        $(this).css({"background-color": "#000","color":"#fff"});
+    });
+
+    /* 二级标题样式 */
+    $(".stair2 > div > div").on("click",function(){
+        $(".stair2   > div > div").css({"background-color": "#27eaff","color":"#000"});
+        $(this).css({"background-color": "#000","color":"#fff"});
+    });
+});
